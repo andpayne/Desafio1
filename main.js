19f27390448574b28aa6d128dede813bc88fe41a
Andres Pace <andpace89@gmail.com>
2023-08-05 18:53:37 -0300

    Desafio 1

--- main.js
+++ main.js
@@ -1,46 +1,67 @@
+// La clase ProductManager administra los productos que van a ser visualizados.
 class ProductManager {
     constructor() {
       this.products = []; 
-      this.nextProductId = 1; 
+    }
+
+    addProducts(product) {
+      const prod = this.products.find(prod => prod.code === prod.code)
+
+      if(prod){
+        console.log("Producto ya encontrado")
+      } else {
+        this.products.push(product)
+      }
+    } 
+
+    getProducts() {
+      console.log(this.products)
+    }
+
+    getProductById(id) {
+      const prod = this.products.find (prod => prod.id === id)
+
+      if(prod) {
+        console.log(prod)
+      } else {
+        console.log("Producto no encontrado")
+      }
     }
 }
 
-addProduct(title, description, price, thumbnail, code, stock) {
-        if (!title || !description || !price || !thumbnail || !code || !stock) {
-            throw new Error("Deben completarse los campos obligatorios.");
-        }
-            const codeExists = this.products.some(product => product.code === code);
-            if (codeExists) {
-            throw new Error("El codigo del producto se repite.");
-            }
-        
-            const product = {
-            title: title,
-            description: description,
-            price: price,
-            thumbnail: thumbnail,
-            code: code,
-            stock: stock,
-            };
-        
-        this.products.push(product);
-        this.nextProductId++;
-    }    
-      
-    getProductById(productId) {
-        const product = this.products.find(product => product.id === productId);
-        if (product) {
-          return product;
-        } else {
-          console.log("Not Found");
-        }
-    }  
-        
-    getProducts() {
-            return this.products;
+// Creo clase Product para tener un control individual de los productos. 
+class Product {
+  constructor (title, description, price, thumbnail, code, stock) {
+    this.title = title 
+    this.description = description
+    this.price = price 
+    this.thumbnail = thumbnail 
+    this.code = code 
+    this.stock = stock 
+    this.id = Product.incrementarId() // Mi Id es el resultado de lo que devuelva este metodo. 
+  }
+  // Defino un metodo de CLASE 
+  static incrementarId () {
+    //Si existe esta propiedad, la auemento en 1 o la creo. 
+    if(this.idIncrement) {
+      this.idIncrement++
+    } else {
+      this.idIncrement = 1 
     }
+    return this.idIncrement
+  }
+}
 
+const producto1 = new Product("Arroz", "Rico", 300, "AA123LE", 20, [])
+const producto2 = new Product("Lentejas", "Rico", 300, "AA123LE", 20, [])
+const producto3 = new Product("Garbanzos", "Rico", 300, "AA123LE", 20, [])
 
-      
+const productManager = new ProductManager()
 
-      
+productManager.addProducts(producto1)
+productManager.addProducts(producto2)
+productManager.addProducts(producto3)
+
+productManager.getProducts()
+
+productManager.getProductById(2)
